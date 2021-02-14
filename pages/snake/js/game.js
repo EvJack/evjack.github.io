3810657a8aca0494257d95393409de6ff20888f7
Eugene <50021816+EvJack@users.noreply.github.com>
2021-02-14 21:51:21 +0700

    delete

--- pages/snake/js/game.js
+++ pages/snake/js/game.js
@@ -1,340 +1,219 @@
-class Vec {
-  constructor(x = 0, y = 0) {
-      this.x = x;
-      this.y = y;
-  }
+/* eslint-disable linebreak-style */
+/* eslint-disable brace-style */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+// Поле, на котором всё будет происходить, — тоже как бы переменная
+
+const canvas = document.getElementById('game');
+
+// Классическая змейка — двухмерная, сделаем такую же
+
+const context = canvas.getContext('2d');
+
+// Размер одной клеточки на поле — 16 пикселей
+
+const grid = 16;
+
+// Служебная переменная, которая отвечает за скорость змейки
+
+let count = 0;
+
+// А вот и сама змейка
+
+const snake = {
+  // Начальные координаты
+
+  x: 160,
+
+  y: 160,
+
+  // Скорость змейки — в каждом новом кадре змейка смещается по оси Х или У. На старте будет двигаться горизонтально, поэтому скорость по игреку равна нулю.
+
+  dx: grid,
+
+  cells: [],
+
+  // Стартовая длина змейки — 4 клеточки
+
+  maxCells: 2,
+};
+
+// А это — еда. Представим, что это красные яблоки.
+
+const apple = {
+  // Начальные координаты яблока
+
+  x: 320,
+
+  y: 320,
+};
+
+// Делаем генератор случайных чисел в заданном диапазоне
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-class Rect { 
-  constructor(w, h) {
-      this.pos = new Vec;
-      this.size = new Vec(w, h);
+// Игровой цикл — основной процесс, внутри которого будет всё происходить
+
+function loop() {
+  // Хитрая функция, которая замедляет скорость игры с 60 кадров в секунду до 15 (60/15 = 4)
+
+  requestAnimationFrame(loop);
+
+  // Игровой код выполнится только один раз из четырёх, в этом и суть замедления кадров, а пока переменная count меньше четырёх, код выполняться не будет
+
+  if (++count < 6) {
+    return;
   }
+
+  // Обнуляем переменную скорости
+
+  count = 0;
+
+  // Очищаем игровое поле
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Двигаем змейку с нужной скоростью
+
+  snake.x += snake.dx;
+
+  snake.y += snake.dy;
+
+  // Если змейка достигла края поля по горизонтали — продолжаем её движение с противоположной строны
+
+  if (snake.x < 0) {
+    snake.x = canvas.width - grid;
+  } else if (snake.x >= canvas.width) {
+    snake.x = 0;
+  }
+
+  // Делаем то же самое для движения по вертикали
+
+  if (snake.y < 0) {
+    snake.y = canvas.height - grid;
+  } else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
+
+  // Продолжаем двигаться в выбранном направлении. Голова всегда впереди, поэтому добавляем её координаты в начало массива, который отвечает за всю змейку
+
+  snake.cells.unshift({ x: snake.x, y: snake.y });
+
+  // Сразу после этого удаляем последний элемент из массива змейки, потому что она движется и постоянно освобождает клетки после себя
+
+  if (snake.cells.length > snake.maxCells) {
+    snake.cells.pop();
+  }
+
+  // Рисуем еду — красное яблоко
+
+  context.fillStyle = 'red';
+
+  context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
+
+  // Одно движение змейки — один новый нарисованный квадратик
+
+  context.fillStyle = 'green';
+
+  // Обрабатываем каждый элемент змейки
+
+  snake.cells.forEach((cell, index) => {
+    // Чтобы создать эффект клеточек, делаем зелёные квадратики меньше на один пиксель, чтобы вокруг них образовалась чёрная граница
+
+    context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+
+    // Если змейка добралась до яблока...
+
+    if (cell.x === apple.x && cell.y === apple.y) {
+      // увеличиваем длину змейки
+
+      snake.maxCells++;
+
+      // Рисуем новое яблочко
+
+      // Помним, что размер холста у нас 400x400, при этом он разбит на ячейки — 25 в каждую сторону
+
+      apple.x = getRandomInt(0, 25) * grid;
+
+      apple.y = getRandomInt(0, 25) * grid;
+    }
+
+    // Проверяем, не столкнулась ли змея сама с собой
+
+    // Для этого перебираем весь массив и смотрим, есть ли у нас в массиве змейки две клетки с одинаковыми координатами
+
+    for (let i = index + 1; i < snake.cells.length; i++) {
+      // Если такие клетки есть — начинаем игру заново
+
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        // Задаём стартовые параметры основным переменным
+
+        snake.x = 160;
+
+        snake.y = 160;
+
+        snake.cells = [];
+
+        snake.maxCells = 2;
+
+        snake.dx = grid;
+
+        snake.dy = 0;
+
+        // Ставим яблочко в случайное место
+
+        apple.x = getRandomInt(0, 25) * grid;
+
+        apple.y = getRandomInt(0, 25) * grid;
+      }
+    }
+  });
 }
 
-class Snake extends Rect {
-  constructor(w, h) {
-      super(w, h);
-      this.vel = new Vec;
-      this.direction = new Vec;
-      this.currentTailSize = 0;
-      this.currentSegment = -1;
-      this.maxTailSize = 10;
-      this.tail = [];
+// Смотрим, какие нажимаются клавиши, и реагируем на них нужным образом
+
+document.addEventListener('keydown', (e) => {
+  // Дополнительно проверяем такой момент: если змейка движется, например, влево, то ещё одно нажатие влево или вправо ничего не поменяет — змейка продолжит двигаться в ту же сторону, что и раньше. Это сделано для того, чтобы не разворачивать весь массив со змейкой на лету и не усложнять код игры.
+
+  // Стрелка влево
+
+  // Если нажата стрелка влево, и при этом змейка никуда не движется по горизонтали…
+
+  if (e.which === 37 && snake.dx === 0) {
+    // то даём ей движение по горизонтали, влево, а вертикальное — останавливаем
+
+    // Та же самая логика будет и в остальных кнопках
+
+    snake.dx = -grid;
+
+    snake.dy = 0;
   }
 
-  addTailSegment() {
-      this.currentSegment++;
-      this.currentTailSize++;
-      this.tail.push(new Rect(this.size.x, this.size.y));
-  }
-  
-  setPosition(coordinate, position) {
-      this.pos[coordinate] = position;
+  // Стрелка вверх
+  else if (e.which === 38 && snake.dy === 0) {
+    snake.dy = -grid;
+
+    snake.dx = 0;
   }
 
-  setTailSegmentPosition(segment, coordinate, position) {
-      this.tail[segment].pos[coordinate] = position;
+  // Стрелка вправо
+  else if (e.which === 39 && snake.dx === 0) {
+    snake.dx = grid;
+
+    snake.dy = 0;
   }
 
-  changeMovementDirection(movement) {
-      switch(movement) {
-          case 'up':
-              this.direction.y = -1;
-              this.direction.x = 0;
-              break;
-          case 'down':
-              this.direction.y = 1;
-              this.direction.x = 0;
-              break;
-          case 'left':
-              this.direction.x = -1;
-              this.direction.y = 0;
-              break;
-          case 'right':
-              this.direction.x = 1;
-              this.direction.y = 0;
-              break;
-          default:
-              throw new Error('Inappropriate value');
-      }
-  }
-}
+  // Стрелка вниз
+  else if (e.which === 40 && snake.dy === 0) {
+    snake.dy = grid;
 
-class Food extends Rect {
-  constructor(size, canvasWidth, canvasHeight, cellSize) {
-      super(size, size);
-      this.canvasWidth = canvasWidth;
-      this.canvasHeight = canvasHeight;
-      this._cellSize = cellSize;
-  }
-
-  _getRandomInt(min, max, num) {
-      return Math.floor(Math.floor(Math.random() * (max - min) + min) / num) * num;
-  }
-
-  generateRandomPos() {
-      // this.pos.x = 0;
-      // this.pos.y = 0;
-      this.pos.x = this._getRandomInt(0, this.canvasWidth - this.size.x, this._cellSize);
-      this.pos.y = this._getRandomInt(0, this.canvasHeight - this.size.y, this._cellSize);
-  }
-}
-
-class Game {
-  constructor() {
-      this._canvas = document.querySelector('#canvas');
-      this._ctx = this._canvas.getContext('2d');
-      this._canvasWidth = this._canvas.width;
-      this._canvasHeight = this._canvas.height;
-      this.over = false;
-
-      this.snake = new Snake(10, 10);
-      this.food = new Food(this.snake.size.x, this._canvasWidth, this._canvasHeight, 10);
-      
-      this.snakeVelocity = 10;
-      this.stepDelay = 100;
-      
-      this.snake.vel.x = this.snakeVelocity;
-      this.snake.vel.y = this.snakeVelocity;
-
-      this.start = () => {
-          if(!this.over) {
-              this.update();
-              setTimeout(() => {
-                  window.requestAnimationFrame(this.start)
-              }, this.stepDelay);
-          } else {
-              this.showGameOverText() 
-          }
-      }
-  }
-
-  draw() {
-      this._ctx.fillStyle = '#000';
-      this._ctx.fillRect(0, 0, this._canvasWidth, this._canvasHeight);
-      
-      this._ctx.fillStyle = '#fff';
-      this._ctx.fillRect(this.snake.pos.x, this.snake.pos.y, this.snake.size.x, this.snake.size.y);    
-      
-      this.drawSnakeTail();
-      // this.showGrid();
-      this._ctx.fillStyle = '#30e861';
-      this._ctx.fillRect(this.food.pos.x, this.food.pos.y, this.food.size.x, this.food.size.y);
-  }
-  
-  drawSnakeTail() {
-      this.snake.tail.forEach((segment) => {
-          this._ctx.fillStyle = '#fff';
-          this._ctx.fillRect(segment.pos.x, segment.pos.y, segment.size.x, segment.size.y);
-      })
-  }
-
-  moveTailTo(direction) {
-      this.tailDelay = 0;
-      this.snake.tail.forEach((segment, i) => {
-          setTimeout(() => {
-              switch(direction) {
-                  case 'left':
-                      segment.pos.x -= this.snake.vel.x;
-                      break;
-                  case 'right':
-                      segment.pos.x += this.snake.vel.x;
-                      break;
-                  case 'top':
-                      segment.pos.y -= this.snake.vel.y;
-                      break;
-                  case 'bottom':    
-                      segment.pos.y += this.snake.vel.y;
-                      break;
-                  case 'leftEdge':
-                      segment.pos.x = this._canvasWidth;
-                      break;
-                  case 'rightEdge':
-                      segment.pos.x = -this.snake.size.x;
-                      break;
-                  case 'topEdge':
-                      segment.pos.y = -this.snake.size.y;
-                      break;
-                  case 'bottomEdge':
-                      segment.pos.y = this._canvasHeight;
-                      break;
-                  default:
-                      return;
-              }
-          }, this.tailDelay += this.stepDelay);
-      });
-  }
-
-  moveSnakeTo(direction) {
-      switch(direction) {
-          case 'right':
-              this.snake.pos.x += this.snake.vel.x;
-              break;
-          case 'left':
-              this.snake.pos.x -= this.snake.vel.x;
-              break;
-          case 'top':
-              this.snake.pos.y -= this.snake.vel.y;
-              break;
-          case 'bottom':
-              this.snake.pos.y += this.snake.vel.y;
-              break;
-          default:
-              break;
-      }
-  }
-
-  edgeToEdgeMovement() {
-      if(this.snake.pos.x > this._canvasWidth - this.snake.size.x && this.snake.direction.x === 1) {
-          this.snake.setPosition('x', -this.snake.size.x);
-          this.moveTailTo('rightEdge');
-      } else if(this.snake.pos.x < 0 && this.snake.direction.x === -1) {
-          this.snake.setPosition('x', this._canvasWidth);
-          this.moveTailTo('leftEdge');
-      } else if(this.snake.pos.y > this._canvasHeight - this.snake.size.y && this.snake.direction.y === 1) {
-          this.snake.setPosition('y', -this.snake.size.y);
-          this.moveTailTo('topEdge');
-      } else if(this.snake.pos.y < 0 && this.snake.direction.y === -1) {
-          this.snake.setPosition('y', this._canvasHeight);
-          this.moveTailTo('bottomEdge');
-      } else {
-          return null;
-      }
-  }
-
-  controlSnakeMovement() {
-      if(Math.sign(this.snake.direction.x) === 1) {
-          this.moveSnakeTo('right');
-          this.moveTailTo('right');
-      } else if(Math.sign(this.snake.direction.x) === -1) {
-          this.moveSnakeTo('left');
-          this.moveTailTo('left');
-      } else if(Math.sign(this.snake.direction.y) === 1) {
-          this.moveSnakeTo('bottom');
-          this.moveTailTo('bottom');
-      } else if(Math.sign(this.snake.direction.y) === -1) {
-          this.moveSnakeTo('top');
-          this.moveTailTo('top');
-      }
-  }
-
-  update() {
-      this.edgeToEdgeMovement();
-      this.controlSnakeMovement();
-
-      if(!this.defineCollisionWithFood()) {
-          this.defineCollisionWithSnake();
-      } else {
-          this.defineCollisionWithFood()
-      }
-      
-      this.draw();
-  }
-
-  defineCollisionWithFood() {
-      if(this.snake.pos.x === this.food.pos.x && this.snake.pos.y === this.food.pos.y) {
-          this.snake.addTailSegment();
-          this.snake.setTailSegmentPosition(this.snake.currentSegment, 'x', this.snake.pos.x);
-          this.snake.setTailSegmentPosition(this.snake.currentSegment, 'y', this.snake.pos.y);
-          this.food.generateRandomPos();
-          return true;
-      } else {
-          return false;
-      }
-  }
-
-  defineCollisionWithSnake() {
-      let tail = this.snake.tail;
-      for(let i = 0; i < tail.length; i++) {
-          if((this.snake.pos.x === tail[i].pos.x && this.snake.pos.y === tail[i].pos.y)) {
-              console.log('Game over!');
-              this.over = true;
-          }
-      }
-  }
-
-  //Decore methods
-  showGrid() {
-      this.rows = this._canvasWidth / this.snakeVelocity;
-      this.cols = this._canvasHeight / this.snakeVelocity;
-
-      const createRows = () => {
-          for(let i = 0; i < this._canvasHeight; i += this.snakeVelocity) {
-              this._ctx.beginPath();
-              this._ctx.moveTo(0, i);
-              this._ctx.lineTo(this._canvasWidth, i);
-              this._ctx.strokeStyle = 'red';
-              this._ctx.stroke();
-          }
-      }
-      
-      const createCols = () => {
-          for(let i = 0; i < this._canvasWidth; i += this.snakeVelocity) {
-              this._ctx.beginPath();
-              this._ctx.moveTo(i, 0);
-              this._ctx.lineTo(i, this._canvasHeight);
-              this._ctx.strokeStyle = 'red'
-              this._ctx.stroke();
-          }
-      }
-
-      createRows();
-      createCols();
-  }
-
-  showGameOverText() {
-      const textSize = 40;
-      const fontFamily = 'Arial'
-      const text = 'Game Over!';
-      this._ctx.fillStyle = 'red';
-      this._ctx.font = `${textSize}px ${fontFamily}`;
-      this._ctx.fillText(text, this._canvasWidth / 2 - Math.ceil(this._ctx.measureText(text).width / 2), this._canvasHeight / 2 + Math.ceil(textSize / 3));
-  }
-
-  restart() {
-      if(this.over) {
-          this.over = false;
-          this.snake.tail = [];
-          this.snake.setPosition('x', 0);
-          this.snake.setPosition('y', 0);
-          this.snakeVelocity = 10;
-          this.snake.direction = new Vec;
-          this.snake.currentTailSize = 0;
-          this.snake.currentSegment = -1;
-          this.snake.maxTailSize = 10;
-          
-          this.food.generateRandomPos();
-          this.start()
-      }
-  }
-}
-
-const game = new Game;
-game.food.generateRandomPos();
-game.start();
-
-const restartBtn = document.querySelector('.restart');
-
-document.addEventListener('keypress', (event) => {
-  switch(event.code) {
-      case 'KeyW': //w
-          game.snake.changeMovementDirection('up');
-          break;
-      case 'KeyS': //s
-          game.snake.changeMovementDirection('down');
-          break;
-      case 'KeyA': //a
-          game.snake.changeMovementDirection('left');
-          break;
-      case 'KeyD': //d
-          game.snake.changeMovementDirection('right');
-          break;
-      default:
-          break;
+    snake.dx = 0;
   }
 });
 
-restartBtn.addEventListener('click', (event) => {
-  game.restart();
-})
+// Запускаем игру
+
+requestAnimationFrame(loop);
